68584d436308cd25378a2a06a2727b1def51b378
Ethan Ennis <ethan_ennis_8@icloud.com>
2022-03-20 21:05:51 -0400

    Initial Commit

---
+++ MidtermCSC416/MidtermCSC416App.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermCSC416App.swift
+//  MidtermCSC416
+//
+//  Created by Ethan Ennis on 3/20/22.
+//
+
+import SwiftUI
+
+@main
+struct MidtermCSC416App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
